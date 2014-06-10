791241e103c54596252a0eb9fb98fa39039429a8
Emil Kilhage <emilkilhage@gmail.com>
2014-06-10 14:31:31 +0200

    Created base for the generate:yaml:definitions

---
+++ src/DRI/SugarCRM/Console/Command/Generate/Yaml/DefinitionsCommand.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace DRI\SugarCRM\Console\Command\Generate\Yaml;
+
+use \DRI\SugarCRM\Console\Command\ApplicationCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * @author Emil Kilhage
+ */
+class DefinitionsCommand extends ApplicationCommand
+{
+
+    protected function configure()
+    {
+        $this->setName("generate:yaml:definitions")
+            ->addArgument("moduleName", InputArgument::REQUIRED, "The name of the module that you want to add the yaml vardef file for")
+            ->addArgument("fileName", InputArgument::OPTIONAL, "The name of the yaml vardef file that you want to add")
+            ->setDescription("Generates yaml definition files into a module (Not implemented)");
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     *
+     * @return int|null|void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+
+    }
+
+}
